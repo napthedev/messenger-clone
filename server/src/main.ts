b8da518a47ab76f0327e9b2bdf6eace2886a9a7d
napthedev <phongna.dev@gmail.com>
2023-04-17 09:27:19 +0700

    feat: listen on PORT variable

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,6 +5,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
